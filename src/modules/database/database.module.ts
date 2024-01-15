de608c092d750ccca22cf38282b7c05c741622f1
Adama THIAW <adama.thiaw@paps-app.com>
2024-01-15 22:32:38 +0000

    format with prettier

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,15 +1,15 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from 'nestjs-prisma';
-import { DatabaseServiceFactory } from './database.service';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "nestjs-prisma";
+import { DatabaseServiceFactory } from "./database.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
     PrismaModule.forRootAsync({
       isGlobal: true,
       useClass: DatabaseServiceFactory,
-      imports: [ConfigModule],
-    }),
-  ],
+      imports: [ConfigModule]
+    })
+  ]
 })
 export class DatabaseModule {}
